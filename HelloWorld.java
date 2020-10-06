dc5f6f944cbf84d596b7a19d01ba6037b2a87fde
fachavarro <51423682+fachavarro@users.noreply.github.com>
2020-10-06 08:28:05 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
         System.out.println("Hello, World Microservices course...");
+    System.out.println("Hello, Hola de nuevo!");
   }
 }
 
